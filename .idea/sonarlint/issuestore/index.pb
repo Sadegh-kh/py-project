a10fff04465f3c1ddb693fe47742c19ff1843030
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-26 23:28:03 +0330

    init greedy algorithms for set-covering problem

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,16 @@ E
 test/test.py,1\e\1edd94c495429b0a9467c42ca92ad59aa0b42876
 ;
 lambda/1.py,3\b\3bbc155e7d0d0a80ac3d1b7574d8616d10651977
+=
+algo/queue.py,9\d\9d6e71a0bf4132d235f82d8f0adf5bfbf1682b24
+=
+algo/graph.py,9\4\946d37ea3f91648c6e2492c43c6e9874cc51bb29
+@
+algo/dijkesta.py,9\b\9b3acb1c0fe2396e6c2dddd4c6091243ba335c66
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
 7
 main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
+A
+test/func_test.py,8\8\884699d916989cba0907e4040a2c6e84766e9c70
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-b = open(r"note2.txt", "r+",encoding="utf-8")
+b = open(r"note2.txt", "r+", encoding="utf-8")
 print(b.read())
 import random
-s="sadegh"
+
+s = "sadegh"
 print(s.startswith("s"))
-a=['a','s',1]
+a = ["a", "s", 1]
 a.reverse()
-a.insert(1,'s')
+a.insert(1, "s")
 print(a)
 a.remove("s")
-a.insert(1,'1')
+a.insert(1, "1")
 print(a)
 a.pop()
 print(a)
-print(random.uniform(100,53242))
-print(random.sample(a,2))
+print(random.uniform(100, 53242))
+print(random.sample(a, 2))
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+print([BASE_DIR])
+print(BASE_DIR)
