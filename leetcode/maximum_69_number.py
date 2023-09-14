34b31c7ed29373bd780ffa5c692022bccd356e00
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-09-14 23:01:29 +0330

    solve maximum 69 greedy problem

---
+++ leetcode/maximum_69_number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximum_69_number(self, num: int) -> int:
+        # method 1
+        # checked = 0
+        # new_num = ""
+        # for i in str(num):
+        #     if i == "6" and checked == 0:
+        #         new_num += "9"
+        #         checked = 1
+        #         continue
+        #     new_num += i
+        # return int(new_num)
+
+        # method 2
+        return int(str(num).replace("6", "9", 1))
+
+
+# s = Solution()
+# print(s.maximum_69_number(9669))
+num = "96679"
+finded_first_6 = num.find("6")
+
+
+print(num.replace("6", "9", 1))
